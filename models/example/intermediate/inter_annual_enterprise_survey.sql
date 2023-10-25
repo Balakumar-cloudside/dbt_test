5fb252b9afb48602e5042750c85ca6f851e14dd7
Balakumar-cloudside <119562666+Balakumar-cloudside@users.noreply.github.com>
2023-10-25 18:26:41 +0530

    Update inter_annual_enterprise_survey.sql

--- models/example/intermediate/inter_annual_enterprise_survey.sql
+++ models/example/intermediate/inter_annual_enterprise_survey.sql
@@ -1,12 +1,11 @@
 with
-
 {{config(materialized='view')}}
 
 inter_survey as (
 SELECT
      SUM(CAST(t2.value AS INT64)) AS Total_Value,
-     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey01'), except=["id"]) }},
-     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey02'), except=["id"]) }}
+     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey01'), except=["id","value"]) }},
+     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey02'), except=["id","value"]) }}
         
 FROM
      {{ ref('stg_annual_enterprise_survey02') }} AS t2
@@ -26,5 +25,5 @@ GROUP BY
       Variable_category,
       Industry_aggregation_NZSIOC
 )
-		
+
 Select * from inter_survey
