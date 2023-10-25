e2dea58da641249792e5ea378fc10afd376a4166
Balakumar-cloudside <119562666+Balakumar-cloudside@users.noreply.github.com>
2023-10-25 18:18:00 +0530

    Update stg_annual_enterprise_survey01.sql

--- models/example/Staging/stg_annual_enterprise_survey01.sql
+++ models/example/Staging/stg_annual_enterprise_survey01.sql
@@ -5,7 +5,7 @@ SELECT * FROM `cloudside-academy.dbt_sample_data.annual-enterprise-survey01`),
 ae AS ( 
 SELECT 
     id,
-    Value,
+    value,
     Industry_aggregation_NZSIOC,
     Variable_category
 FROM survey
