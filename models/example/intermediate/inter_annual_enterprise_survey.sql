335de2339cc62313abd3420ed80ad58926b6309b
Balakumar-cloudside <119562666+Balakumar-cloudside@users.noreply.github.com>
2023-10-25 16:23:51 +0530

    Create inter_annual_enterprise_survey.sql

---
+++ models/example/intermediate/inter_annual_enterprise_survey.sql
@@ -0,0 +1,30 @@
+with
+
+{{config(materialized='ephemeral')}}
+
+inter_survey as
+SELECT
+     SUM(CAST(t2.value AS INT64)) AS Total_Value,
+     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey01'), except=["id"]) }},
+     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey02'), except=["id"]) }},
+        
+FROM
+     {{ ref('stg_annual_enterprise_survey02') }} AS t2
+RIGHT JOIN
+     {{ ref('stg_annual_enterprise_survey01') }} AS t1
+ON
+    t2.id = t1.id
+WHERE
+    t2.value <> 'C' AND t1.value <> 'C'
+GROUP BY
+      Year, 
+      Industry_code, 
+      Industry_name, 
+      Size_group, 
+      Variable, 
+      Units,
+      Variable_category,
+      Industry_aggregation_NZSIOC;
+)
+		
+Select * from inter_survey
